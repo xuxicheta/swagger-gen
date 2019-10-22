f8c6a30937db51b8d1a4d38b3c6d5dd27b786c0b
Anton Shvets <antshvets@gmail.com>
2019-10-22 19:00:17 +0300

    buildfix

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,12 @@
+import { resolve } from 'path';
 import { SwaggerGen } from './swagger-gen';
 
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
 
-const swaggerGen = new SwaggerGen();
+
+
+const modelsDirPath = resolve(__dirname, '../..', 'models');
+const templateDirPath = resolve(__dirname, '../..', 'mustache');
+
+const swaggerGen = new SwaggerGen(modelsDirPath, templateDirPath);
 swaggerGen.run();
