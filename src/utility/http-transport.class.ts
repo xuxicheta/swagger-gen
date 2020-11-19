90fbe26be6f8f187612863669361346aa18355fb
Anton Shvets <shvets@corp.satel.org>
2020-11-19 15:37:32 +0200

    remove request

--- src/utility/http-transport.class.ts
+++ src/utility/http-transport.class.ts
@@ -1,14 +1,43 @@
-import { get, Response } from 'request';
+import { get } from 'http';
 
 export class HttpTransport {
   public requestObject<T>(url: string): Promise<T> {
     console.log(`getting json from ${url} ...`);
     return new Promise<T>((resolve: (value: T) => void, reject: (error: Error) => void) => {
-      get(url, (err: Error, response: Response, body: string) => {
-        return err
-          ? reject(err)
-          : resolve(JSON.parse(body));
-       });
+      get(url, (res) => {
+        const { statusCode } = res;
+        const contentType = res.headers['content-type'];
+
+        const error = (() => {
+          if (statusCode !== 200) {
+            return new Error(`Request Failed. Status Code: ${statusCode}`);
+          }
+          if (!/^application\/json/.test(contentType)) {
+            return new Error(`Invalid content-type. Expected application/json but received ${contentType}`);
+          }
+        })();
+
+        if (error) {
+          reject(error);
+        }
+
+        res.setEncoding('utf8');
+        let rawData = '';
+        res.on('data', (chunk) => {
+          rawData += chunk;
+        });
+        res.on('end', () => {
+          try {
+            const parsedData = JSON.parse(rawData);
+            resolve(parsedData);
+          } catch (e) {
+            console.error(e.message);
+          }
+        });
+      })
+        .on('error', (e) => {
+          reject(new Error(`Got error: ${e.message}`));
+        });
     });
   }
 }
